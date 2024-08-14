12de08407ee1daae2507b9312c9bb5c29722dd9c
Miroslav Mitev <m1r0@users.noreply.github.com>
2024-08-14 17:12:21 +0300

    Implement the rest of the static main page

--- src/static/main.php
+++ src/static/main.php
@@ -13,15 +13,21 @@
 		<h1>Migrate your site to WordPress.com</h1>
 		<p>Get ready for better speed, security, and support. Enter your email address so that we can send you notifications as we migrate your site to WordPress.com.</p>
 
-		<form action="">
-			<div class="wpcom-migration-input-group">
-				<label for="wpcom-migration-email">Email Address</label>
-				<input type="text" placeholder="Enter your email address" id="wpcom-migration-email">
+		<form class="wpcom-migration-form">
+			<div class="wpcom-migration-section">
+				<div class="wpcom-migration-input-group">
+					<label for="wpcom-migration-email">Email address</label>
+					<input type="text" placeholder="Enter your email address" id="wpcom-migration-email">
+				</div>
+
+				<div class="wpcom-migration-input-group wpcom-migration-input-group--checkbox">
+					<input type="checkbox" id="wpcom-migration-terms">
+					<label for="wpcom-migration-terms">I agree to BlogVault’s <a href="">Terms & Conditions</a> and <a href="">Privacy Policy</a></label>
+				</div>
 			</div>
 
-			<div class="wpcom-migration-input-group">
-				<input type="checkbox" id="wpcom-migration-terms">
-				<label for="wpcom-migration-terms">I agree to BlogVault’s <a href="">Terms & Conditions</a> and <a href="">Privacy Policy</a></label>
+			<div class="wpcom-migration-section">
+				<button type="submit">Continue</button>
 			</div>
 		</form>
 	</main>
@@ -29,5 +35,12 @@
 	<aside class="wpcom-migration-sidebar">
 		<h3>Too busy to do this now?</h3>
 		<p>Let us do it for you. Our migration service is Free and only takes up to 2 business days. Plus, we’ll give you a one time offer of 50% off your first year of hosting.</p>
+
+		<a class="wpcom-migration-cta-link" href="https://wordpress.com/move/" target="_blank">Get your Free migration</a>
+
+		<div class="wpcom-migration-testimonial">
+			<div class="wpcom-migration-testimonial__text">100% loved by our customers</div>
+			<img class="wpcom-migration-testimonial__image" src="<?php echo esc_url( plugins_url('../static/assets/images/testimonial.png', __FILE__ ) ); ?>" alt="testimonial" />
+		</div>
 	</aside>
 </div>

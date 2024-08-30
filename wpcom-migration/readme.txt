ab38a198cb4ea789130a6214f3898dbecde3885c
Shivam Sharma <shivamsharma@blogvault.net>
2024-08-30 14:59:04 +0300

    Moving the content from src folder to wpcom-migration folder

---
+++ wpcom-migration/readme.txt
@@ -0,0 +1,23 @@
+=== WordPress.com Migration ===
+Contributors: wordpress.com, blogvault, akshatc
+Tags: wordpress.com, migration
+Requires at least: 4.0
+Tested up to: 6.6
+Requires PHP: 5.6.0
+Stable tag: 5.65
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+The WordPress.com Migration plugin makes migrating sites to the WordPress.com platform effortless. Whether you are a developer with tons of experience moving sites or have never moved a site before, this plugin does all the hard work so that you don't have to. Move as many sites as you need, quickly and with minimal interaction.
+
+== FREQUENTLY ASKED QUESTIONS ==
+
+=Why do you need my email?=
+We require an email address to send you updates on the migration process, notify you of any errors that occur during the migration.
+
+== CHANGELOG ==
+= 5.63 =
+* Releasing the WordPress.com migration plugin into the WordPress repository.
+
---
+++ wpcom-migration/static/main.php
@@ -0,0 +1,54 @@
+<header class="wpcom-migration-header">
+	<div class="wpcom-migration-header__wpcom-logo">
+		<span class="dashicons dashicons-wordpress-alt"></span>
+	</div>
+
+	<div class="wpcom-migration-header__blogvault-logo">
+		Powered by <img src="<?php echo esc_url( plugins_url('../assets/img/blogvault-logo.png', __FILE__ ) ); ?>" alt="blogvault-logo">
+	</div>
+</header>
+
+<div class="wpcom-migration-container">
+	<main class="wpcom-migration-content">
+		<h1>Migrate your site to WordPress.com</h1>
+		<p>Get ready for better speed, security, and support. Just drop your email below to get started and we'll keep you updated throughout the migration process.</p>
+
+		<form class="wpcom-migration-form">
+			<div class="wpcom-migration-section">
+				<div class="wpcom-migration-input-group">
+					<label for="wpcom-migration-email">Email address</label>
+					<input type="email" placeholder="Enter your email address for updates" id="wpcom-migration-email" required>
+				</div>
+
+				<div class="wpcom-migration-input-group wpcom-migration-input-group--checkbox">
+					<label>
+						<input type="checkbox" id="wpcom-migration-terms" required> I agree to BlogVault’s <a href="">Terms & Conditions</a> and <a href="">Privacy Policy</a>
+					</label>
+				</div>
+			</div>
+
+			<div class="wpcom-migration-section">
+				<button type="submit">Continue</button>
+			</div>
+		</form>
+	</main>
+
+	<aside class="wpcom-migration-sidebar">
+		<div class="wpcom-migration-sidebar__inner">
+			<h3>Let us migrate your site for free</h3>
+			<p>Sit back and our experts will migrate your site for you. You'll get 50% off your first year, and you'll be up and running in no more than 2 business days.</p>
+
+			<div class="wpcom-migration-cta">
+				<a class="wpcom-migration-cta-link" href="https://wordpress.com/move/" target="_blank">
+					Get your Free migration
+				</a>
+				<span class="dashicons dashicons-external"></span>
+			</div>
+
+			<div class="wpcom-migration-testimonial">
+				<div class="wpcom-migration-testimonial__text">Loved by our customers</div>
+				<img class="wpcom-migration-testimonial__image" src="<?php echo esc_url( plugins_url('../static/assets/images/testimonial.png', __FILE__ ) ); ?>" alt="testimonial" />
+			</div>
+		</div>
+	</aside>
+</div>
